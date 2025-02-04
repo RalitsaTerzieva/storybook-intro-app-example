103e1ff37f5d29a3a2ab2f5943c71834be4bfca8
Ralica Popova <ralicaterzieva90@gmail.com>
2025-02-04 15:43:50 +0200

    add first story for large button

--- src/components/ButtonCounter.stories.jsx
+++ src/components/ButtonCounter.stories.jsx
@@ -5,3 +5,12 @@ export default {
     title: 'Counter/ButtonCounter',
     component: ButtonCounter,
 }
+
+// a story where the button size is large
+
+export const Large = {
+    args: {
+        size: 'large',
+        label: 'Click me'
+    }
+}
