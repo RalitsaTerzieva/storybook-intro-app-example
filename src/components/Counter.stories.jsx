85469d61fa2d7ff758341557bf29e51e17009464
Ralica Popova <ralicaterzieva90@gmail.com>
2025-02-12 11:52:22 +0200

    add counter stories

---
+++ src/components/Counter.stories.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { ButtonCounter } from './ButtonCounter';
+import { Counter } from './Counter';
+
+
+export default {
+    title: 'Counter/Counter',
+    component: Counter,
+    tags: ['autodocs'],
+    argTypes: {
+        onClick: {action: 'clicked'},
+        label: { 
+            controls: 'text',
+            table: {defaultValue: {summary: 'Increment'}},
+            description: 'Click for increment the value'
+        }
+    }
+}
+
+export const Default = () => {
+    const [count, setCount] = useState(0);
+
+    const handleCount = () => {
+        setCount(previousCount => previousCount + 1);
+    }
+
+    return (
+        <Counter>
+            <ButtonCounter 
+            onClick={handleCount} 
+            label="increment"
+            backgroundColor="green"
+            buttonVersion="counter-button1"
+            size="large"
+            />
+        </Counter>
+    )
+}
+
+
+
+
